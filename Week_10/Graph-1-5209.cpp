e8f69700b88323ec22520be7fcd4e5ac0bab4be0
damara <damarmuhamad13@gmail.com>
2023-06-15 20:09:02 +0700

    update-graph

--- Week_10/Graph-1-5209.cpp
+++ Week_10/Graph-1-5209.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 const int MAX_SIZE = 100;
 
-int matrix[MAX_SIZE][MAX_SIZE];
-int temp_vertex;
-bool nolsemua;
-
-void buildMatrix() {
+int main() {
+    int temp_vertex, temp_edge, source, objective;
+    bool nolsemua;
+    int matrix[MAX_SIZE][MAX_SIZE]; // Menambahkan deklarasi matriks
+    
+    cout << " Jumlah vertex: ";
+    cin >> temp_vertex;
     for (int i = 1; i <= temp_vertex; i++) {
         for (int j = 1; j <= temp_vertex; j++) {
             matrix[i][j] = 0;
         }
     }
-}
-
-void insertMatrix() {
-    int temp_edge, source, objective;
+    
     cout << " Jumlah tepi: ";
     cin >> temp_edge;
     for (int i = 1; i <= temp_edge; i++) {
@@ -27,45 +26,32 @@ void insertMatrix() {
         matrix[source][objective] = 1;
         matrix[objective][source] = 1;
     }
-}
-
-void traversingGraph() {
+    
     int i = 1;
     bool ketemu;
     nolsemua = false;
-    while (i <= temp_vertex && nolsemua == false) {
+    while (i <= temp_vertex && !nolsemua) {
         int j = 1;
         ketemu = false;
-        while (j <= temp_vertex && ketemu == false) {
+        while (j <= temp_vertex && !ketemu) {
             if (matrix[i][j] == 1) {
                 ketemu = true;
             } else {
                 j++;
             }
         }
-        if (ketemu == false) {
+        if (!ketemu) {
             nolsemua = true;
         } else {
             i++;
         }
     }
-}
-
-void print() {
-    if (nolsemua == true) {
+    
+    if (nolsemua) {
         cout << " Graph tidak terhubung." << endl;
     } else {
         cout << " Graph terhubung." << endl;
     }
-}
-
-int main() {
-    cout << " Jumlah vertex: ";
-    cin >> temp_vertex;
-    buildMatrix();
-    insertMatrix();
-    traversingGraph();
-    print();
     return 0;
 }
 
