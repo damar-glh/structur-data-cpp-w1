3b4db5e2b64e000dc48927fffa853a27f81b992d
damara <damarmuhamad13@gmail.com>
2023-06-15 09:54:12 +0700

    update-task-graph

--- Week_10/Task-Graph-5209.cpp
+++ Week_10/Task-Graph-5209.cpp
@@ -1,1 +1,71 @@
 
+#include <iostream>
+using namespace std;
+
+const int MAX_SIZE = 5;
+
+int matrix[MAX_SIZE][MAX_SIZE];
+int temp_vertex;
+bool nolsemua;
+
+void buildMatrix() {
+    for (int i = 1; i <= temp_vertex; i++) {
+        for (int j = 1; j <= temp_vertex; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+}
+
+void insertMatrix() {
+    int temp_edge, source, objective;
+    cout << " temp of edges: ";
+    cin >> temp_edge;
+    for (int i = 1; i <= temp_edge; i++) {
+        cout << " source vertex " << i << ": ";
+        cin >> source;
+        cout << " objective vertex " << i << ": ";
+        cin >> objective;
+        matrix[source][objective] = 1;
+        matrix[objective][source] = 1;
+    }
+}
+
+void traversingGraph() {
+    int i = 1;
+    bool ketemu;
+    nolsemua = false;
+    while (i <= temp_vertex && nolsemua == false) {
+        int j = 1;
+        ketemu = false;
+        while (j <= temp_vertex && ketemu == false) {
+            if (matrix[i][j] == 1) {
+                ketemu = true;
+            } else {
+                j++;
+            }
+        }
+        if (ketemu == false) {
+            nolsemua = true;
+        } else {
+            i++;
+        }
+    }
+}
+
+void print() {
+    if (nolsemua == true) {
+        cout << " Graph not connected." << endl;
+    } else {
+        cout << " Graph connected." << endl;
+    }
+}
+
+int main() {
+    cout << " temp of vertex: ";
+    cin >> temp_vertex;
+    buildMatrix();
+    insertMatrix();
+    traversingGraph();
+    print();
+    return 0;
+}
